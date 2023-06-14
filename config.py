9dd3fad3a860f1fbb152c7897c6b2b25361ead0c
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-06-14 23:06:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 # Bot token dari @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6028470474:AAE2rpH3pabOpE9ZUvOpR6xeEwZqFbwjYoo")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6028470474:AAG7hld6aCjUsHnoE6eF_GafwJ5v9V7z2oA")
 
 # API ID Anda dari my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "16743442"))
